912a691c58dd5ef0ae1cbb38940c8404d78db54b
thenuga98 <thenugapriya@gmail.com>
2022-01-22 18:41:18 +0400

    Initial commit

---
+++ minimal.py
@@ -0,0 +1,20 @@
+import pygame
+
+def main():
+    print("are we getting here?")
+    pygame.init()
+    logo = pygame.image.load("logo32x32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("Pygame Trial")
+
+    screen = pygame.display.set_mode((240, 180))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+if __name__=="__main__":
+    main()
